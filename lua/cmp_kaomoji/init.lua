f8e1bd5d073cf5559ce591e5a688a46e41e38aef
XiaoXiaoSN <xiao.xiao@tenoz.tw>
2025-01-10 12:31:40 +1100

    feat: add test Ꮚ˘ ꈊ ˘Ꮚ

--- lua/cmp_kaomoji/init.lua
+++ lua/cmp_kaomoji/init.lua
@@ -2,38 +2,38 @@
 local source = {}
 
 source.new = function()
-  local self = setmetatable({}, { __index = source })
-  self.items = nil
-  return self
+	local self = setmetatable({}, { __index = source })
+	self.items = nil
+	return self
 end
 
 ---Return trigger characters for triggering completion (optional).
 source.get_trigger_characters = function()
-  return { ':' }
+	return { ":" }
 end
 
 ---Return the keyword pattern for triggering completion (optional).
 ---If this is omitted, nvim-cmp will use a default keyword pattern. See |cmp-config.completion.keyword_pattern|.
 ---@return string
 source.get_keyword_pattern = function()
-  return [=[\%([[:space:]"'`]\|^\)\zs:[[:alnum:]_\-\+]*:\?]=]
+	return [=[\%([[:space:]"'`]\|^\)\zs:[[:alnum:]_\-\+]*:\?]=]
 end
 
 ---Invoke completion (required).
 ---@param params cmp.SourceCompletionApiParams
 ---@param callback fun(response: lsp.CompletionResponse|nil)
 source.complete = function(self, params, callback)
-  -- Avoid unexpected completion.
-  if not vim.regex(self.get_keyword_pattern() .. '$'):match_str(params.context.cursor_before_line) then
-    return callback()
-  end
+	-- Avoid unexpected completion.
+	if not vim.regex(self.get_keyword_pattern() .. "$"):match_str(params.context.cursor_before_line) then
+		return callback()
+	end
 
-  -- Lazy load kaomoji
-  if not self.items then
-    self.items = require('cmp_kaomoji.kaomoji')()
-  end
+	-- Lazy load kaomoji
+	if not self.items then
+		self.items = require("cmp_kaomoji.kaomoji").setup()
+	end
 
-  callback(self.items)
+	callback(self.items)
 end
 
 return source
---
+++ lua/spec/kaomoji_spec.lua
@@ -0,0 +1,74 @@
+local kaomoji = require("cmp_kaomoji.kaomoji")
+
+describe("Kaomoji Function Tests", function()
+	it("should call _contains successfully", function()
+		local result = kaomoji._contains({ "happy", "joyful" }, "happy")
+		assert.is_true(result, "Expected result to be true")
+
+		local result = kaomoji._contains({ "happy", "joyful" }, "sad")
+		assert.is_false(result, "Expected result to be false")
+	end)
+
+	it("should call setup successfully", function()
+		local result = kaomoji.setup()
+		assert.is_not_nil(result, "Expected result to be not nil")
+
+		print(result)
+	end)
+
+	it("_setup should return kaomoji with 'joyful' added to tags for happy entries", function()
+		local kaomoji_list = {
+			{
+				word = ":cheer_1:",
+				label = "＼(・ω・)/",
+				_tags = { "happy", "hello" },
+			},
+			{
+				word = ":happy_1:",
+				label = "((ヽ(๑╹◡╹๑)ﾉ))♬",
+				_tags = { "music" },
+			},
+			{
+				word = ":naughty_1:",
+				label = "٩(◦`꒳´◦)۶",
+				_tags = { "playful", "mischievous" },
+			},
+			{
+				word = ":fish_1:",
+				label = "<“)))><",
+			},
+		}
+		local synonyms = {
+			happy = { "joyful" },
+		}
+		local expected = {
+			{
+				word = ":cheer_1:",
+				filterText = ":cheer_1: happy hello joyful",
+				label = "＼(・ω・)/",
+				insertText = "＼(・ω・)/",
+			},
+			{
+				word = ":happy_1:",
+				filterText = ":happy_1: music joyful",
+				label = "((ヽ(๑╹◡╹๑)ﾉ))♬",
+				insertText = "((ヽ(๑╹◡╹๑)ﾉ))♬",
+			},
+			{
+				word = ":naughty_1:",
+				filterText = ":naughty_1: playful mischievous",
+				label = "٩(◦`꒳´◦)۶",
+				insertText = "٩(◦`꒳´◦)۶",
+			},
+			{
+				word = ":fish_1:",
+				filterText = ":fish_1:",
+				label = "<“)))><",
+				insertText = "<“)))><",
+			},
+		}
+
+		local result = kaomoji._setup(kaomoji_list, synonyms)
+		assert.is_same(result, expected, "Expected result to be the same as the expected result")
+	end)
+end)
